64ebe2834d3576b11be9c85773d1c4abdc2a813c
Rene Quevedo <>
2023-01-16 11:32:31 -0500

    Added some helper functions to scRNA that calculates proportions per cluster, and subsets the seurat object to a set n size

--- mini_functions/singlecell/scrna.R
+++ mini_functions/singlecell/scrna.R
@@ -87,6 +87,7 @@ getNumOfPcs <- function(seu, min_var=0.9){
 }
 
 ##-- Helper Functions ----
+#---- General helper functions ----
 # Used to subset a seurat object to a set number of cells. It will take
 # a seurat object, split it apart based on a metadata colid, and then
 # select up to n number of cells per list element and return the
@@ -101,6 +102,83 @@ subsetSeu <- function(obj, colid='dataset_celltype', n=1000, seed=1234){
   return(obj2)
 }
 
+#--- Per cluster, estimate difference in proportion of groups ----
+.assignClusterToPartition <- function(seu, clusid, partid, annoid=NULL){
+  # clusid <- 'seurat_clusters'
+  # partid <- 'monocle3_partitions'
+  tbl <- table(seu@meta.data[,clusid], seu@meta.data[,partid])
+  partitions <- apply(tbl / rowSums(tbl), 1, which.max)
+  partitions <- setNames(colnames(tbl)[partitions], names(partitions))
+  
+  prt_clus_df <- as.data.frame(partitions) %>% 
+    tibble::rownames_to_column(., "clusters") %>%
+    arrange(partitions)
+  
+  if(!is.null(annoid)){
+    tbl <- table(seu@meta.data[,clusid], seu@meta.data[,annoid])
+    annos <- apply(tbl / rowSums(tbl), 1, which.max)
+    annos <- setNames(colnames(tbl)[annos], names(annos))
+    
+    prt_clus_df <- prt_clus_df %>% 
+      left_join(as.data.frame(annos) %>% 
+                  tibble::rownames_to_column(., "clusters"),
+                by='clusters')
+  }
+  return(prt_clus_df)
+} 
+
+# Gets the proportion of cells in cluster [clusid] that belong to either 
+# smaple groupings of [sampleid] based on the conditions set by
+# [ident_meta] and [dvar].  For example, proportion of cells
+# per seurat_clusters belonging to samples A,B or C,D, where ident_meta
+# indicates that A,B are GroupX and C,D are group Y
+.getDeltaTbl <- function(seu, clusid, sampleid, dvar, ident_meta, method='rr'){
+  sort_ord <- unique(c(dvar, colnames(ident_meta), 'sampleID'))
+  ident_spl <- ident_meta %>% 
+    dplyr::arrange(!!! rlang::syms(sort_ord)) %>% 
+    group_split(!! rlang::sym(dvar))
+  
+  # sub_ident_meta <- ident_spl[[1]][,sort_ord[c(2,3,1)]]
+  sub_samples <- sapply(ident_spl, function(i) i%>% pull('sampleID'))
+  
+  tbl <- table(list(seu@meta.data[,clusid], seu@meta.data[,sampleid])) %>% 
+    apply(., 2, function(i) i/sum(i)) %>%
+    round(., 3)
+  cluster_hc <- hclust(dist(tbl))$order
+  cluster_hc <- rownames(tbl)[cluster_hc]
+  dtbl <- switch(method,
+                 rr=log2(tbl[,sub_samples[1]] / tbl[,sub_samples[2]]),
+                 dist=tbl[,sub_samples[1]] - tbl[,sub_samples[2]])
+  
+  list("dtbl"=dtbl, "cluster_ord"=cluster_hc, 'tbl'=tbl)
+}
+
+# Wrapper function for annotation and getting the proportion of cells
+# in cluster [clusid] that belong to either smaple groupings of [sampleid] based 
+# on the conditions set by [ident_meta] and [dvar].  For example, 
+# proportion of cells per seurat_clusters belonging to samples A,B or C,D, 
+# where ident_meta indicates that A,B are GroupX and C,D are group Y
+getDiffProportionsPerClusters <- function(
+  obj, clusid='seurat_clusters', sampleid='orig.ident', 
+  delta, ident_meta, annoid='functional.cluster', partid='functional.cluster'
+){
+  # Creates a mapping of cluster to annotations
+  cluster_meta <- .assignClusterToPartition(seu, clusid,partid,annoid)
+  
+  # Get the proportion of GroupA to GroupB per cluster and
+  # format to add in the annotations
+  dtbl_l <- .getDeltaTbl(obj, clusid, sampleid, delta, ident_meta, method='rr')
+  
+  dtbl_melt <- melt(dtbl_l$dtbl) %>%
+    rename_with(., ~c('Delta')) %>%
+    tibble::rownames_to_column(., "Cluster") %>%
+    mutate(Cluster=factor(as.character(Cluster)), 
+           Direction=(Delta>0) %>%
+             if_else(., "Pos","Neg")) %>%
+    left_join(., cluster_meta, by=c('Cluster'='clusters'))
+  dtbl_melt$Cluster <- factor(dtbl_melt$Cluster, levels=dtbl_l$cluster_ord)
+  return(dtbl_melt)
+}
 ##-- Doublet functions ----
 runBcds <- function(seu, bcds.ntop=3000, bcds.srat=1, n=25){
   # Binary classification based doublet scoring (bcds)
