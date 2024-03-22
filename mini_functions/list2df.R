4b068a3cee86b0bd8704f9ae500a198b2115fe45
Rene Quevedo <>
2024-03-22 09:12:13 -0400

    Takes a list and converts it into a dataframe, works recursively

---
+++ mini_functions/list2df.R
@@ -0,0 +1,16 @@
+
+
+list2df <- function(lst, id.col=NULL, value.col=NULL){
+  if(all(sapply(lst, class) == 'list')){
+    lst <- lapply(lst, list2df)
+  }
+  if(all(sapply(lst, class) == 'data.frame')){
+    newcol <- make.unique(c(colnames(lst[[1]]), 'Var'))[ncol(lst[[1]]) + 1]
+    do.call(rbind, lst) %>% 
+      mutate(!!rlang::sym(newcol) := rep(names(lst), sapply(lst, nrow))) %>% 
+      tibble::remove_rownames() 
+  } else if(all(sapply(lst, class) == 'character')) {
+    data.frame("Var1"=rep(names(lst), sapply(lst, length)),
+               "Var2"=as.character(unlist(lst)))
+  }
+}
